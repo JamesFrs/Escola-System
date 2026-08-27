f7d6d8edd6677d2f9925e4e1e1e4290e35a0ffa7
JamesFrs <jamesjosef.fariasf@gmail.com>
2026-08-27 01:34:34 -0400

    Suporte a volume persistente Railway para dados não sumirem

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE = os.path.join(BASE_DIR, 'data', 'escola.db')
+
+# Railway: usa volume persistente em /data se disponível
+RAILWAY_VOLUME = os.environ.get('RAILWAY_VOLUME_MOUNT_PATH', '')
+if RAILWAY_VOLUME:
+    DATABASE = os.path.join(RAILWAY_VOLUME, 'escola.db')
+else:
+    DATABASE = os.path.join(BASE_DIR, 'data', 'escola.db')
+
 SECRET_KEY = os.environ.get('SECRET_KEY', 'escola_eeg_secret_key_2026')
 PORT = int(os.environ.get('PORT', 5000))
